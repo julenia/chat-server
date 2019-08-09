31d2a0caaf6ee6262043dd6a2868cf02854ef771
julenia <julia.jankowska@onet.eu>
2019-08-09 16:37:10 +0200

    database url added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Sse = require('json-sse')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const Sequelize = require('sequelize')
-const databaseUrl = 'postgres://postgres:secret@localhost:5432/postgres'
+const databaseUrl = process.env.DATABASE_URL ||'postgres://postgres:secret@localhost:5432/postgres'
 const db = new Sequelize(databaseUrl)
 
 db
